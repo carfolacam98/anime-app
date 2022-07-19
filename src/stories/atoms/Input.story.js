7a77fddf8e1b9b5bd1bc9e6a9f2a3d01602e4d99
LauCardenas <laura.lcardenas@mercadolibre.com.co>
2022-07-19 07:54:01 -0500

    Fix code indentation

--- src/stories/atoms/Input.story.js
+++ src/stories/atoms/Input.story.js
@@ -1,26 +1,25 @@
-import Input from '../../components/Input'
-import './Input.css'
+import Input from "../../components/Input";
+import "./Input.css";
 
-export default{
- title: "Input",
- component: Input,
-}
+export default {
+  title: "Input",
+  component: Input,
+};
 
-const Template = args => <Input {...args}/>
+const Template = (args) => <Input {...args} />;
 
-export const Text = Template.bind({})
-Text.args={
-    size: "md",
-    type: "text",
-    
-}
-export const Email = Template.bind({})
-Email.args={
-    size: "md",
-    type: "email"
-}
-export const Date = Template.bind({})
-Date.args={
-    size: "md",
-    type: "date"
-}
+export const Text = Template.bind({});
+Text.args = {
+  size: "md",
+  type: "text",
+};
+export const Email = Template.bind({});
+Email.args = {
+  size: "md",
+  type: "email",
+};
+export const Date = Template.bind({});
+Date.args = {
+  size: "md",
+  type: "date",
+};
--- src/stories/molecules/Cards.story.jsx
+++ src/stories/molecules/Cards.story.jsx
@@ -1,28 +1,24 @@
-import Card from "../../components/Cards"
-import './Cards.css'
+import Card from "../../components/Cards";
+import "./Cards.css";
 export default {
   title: "Card",
   component: Card,
   argTypes: {
-    numberOfChildren: { type: "number", defaultValue: 4 },
+    numberOfChildren: { type: "number", defaultValue: 1 },
   },
-}
+};
 const Template = ({ numberOfChildren, ...args }) => (
   <Card {...args}>
-    {[...Array(numberOfChildren).keys()].map(n => (
-      <div
-      >
-        {n + 1}
-      </div>
+    {[...Array(numberOfChildren).keys()].map((n) => (
+      <div>{n + 1}</div>
     ))}
   </Card>
-)
+);
 
-export const Horizontal = Template.bind({})
+export const Horizontal = Template.bind({});
 //mirar la diferencia entre args y props
 Horizontal.args = {
   direction: "row",
   spacing: 2,
   wrap: false,
-}
-
+};
